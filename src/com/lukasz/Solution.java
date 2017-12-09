d6a196f147c80b8b65de88f53dd7d0ec74dc3a07
Lukasz Bany <lbany89@gmail.com>
2017-12-09 12:38:15 +0100

    refactor

--- src/com/lukasz/Solution.java
+++ src/com/lukasz/Solution.java
@@ -3,28 +3,46 @@ package com.lukasz;
 public class Solution {
 
     public static int sumOfCaptcha(String captcha) {
-        int sum = 0;
-        String copyOfCaptcha = captcha;
 
+        int digits[] = fillTableOfDigits(captcha);
+        int sum = sumDigits(digits, captcha);
+
+        return sum;
+    }
+
+    private static int[] fillTableOfDigits(String captcha) {
         int digits[] = new int[captcha.length()];
 
         for(int i = 0; i < captcha.length(); i++) {
             digits[i] = Character.getNumericValue(captcha.charAt(i));
         }
 
+        return digits;
+    }
 
-        for(int i = 0; i < captcha.length() - 1; i++) {
-            if(digits[i] == digits[i+1]) {
-                sum += digits[i];
-            }
-        }
-
-        //There is no explanation what to do if captcha is just one digit like "1".
-        //I think it should by added to sum because it's circular
-        if(captcha.length() > 0 && digits[0] == digits[captcha.length() - 1]) {
-            sum += digits[0];
-        }
+    private static int sumDigits(int[] digits, String captcha) {
+        int sum = 0;
+        sum += sumDigitsFromFirstToLast(digits, captcha);
+        sum += sumFirstAndLastDigit(digits, captcha);
 
         return sum;
+    }
+
+    private static int sumDigitsFromFirstToLast(int[] digits, String captcha) {
+        int result = 0;
+        for(int i = 0; i < captcha.length() - 1; i++) {
+            if(digits[i] == digits[i+1]) {
+                result += digits[i];
+            }
+        }
+        return result;
+    }
+
+    private static int sumFirstAndLastDigit(int[] digits, String captcha) {
+        int result = 0;
+        if(captcha.length() > 0 && digits[0] == digits[captcha.length() - 1]) {
+            result += digits[0];
+        }
+        return result;
     }
 }
